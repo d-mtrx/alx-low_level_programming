c748e0365b9b92f2adb873a3715480d67c67e611
d-mtrx <102062528+d-mtrx@users.noreply.github.com>
2022-06-05 15:37:47 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,23 +6,29 @@
  *
  * Return: Always 0 (Success)
  */
+
+#include <stdio.h>
+
 int main(void)
 {
-	int d;
+	int digit1, digit2;
 
-	for (d = 0; d < 90; d++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for ()
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-		}
-		if (d != 89)
-		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
