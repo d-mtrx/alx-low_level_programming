778f4828d3c73348d219ba19f15024689dc1235b
d-mtrx <102062528+d-mtrx@users.noreply.github.com>
2022-06-05 15:59:55 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
-#include <stdio.h>
-
 int main(void)
 {
 int digit1, digit2;
-
 for (digit1 = 0; digit1 < 9; digit1++)
 {
 	for (digit2 = digit1 + 1; digit2 < 10; digit2++)
